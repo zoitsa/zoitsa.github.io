fed5142cbd3da6c11833b454fba114fe8506cc7e
Zoe C Koulouris <zoeckoulouris@Zoes-MacBook-Pro.local>
2016-10-18 15:24:25 -0400

    testing mobile scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 $(document).ready(function() {
 
-//alert("hey!");
+	if (window.location.hash) { 
+        //bind to scroll function
+        $(document).scroll( function() {
+            var hash = window.location.hash
+            var hashName = hash.substring(1, hash.length);
+            var element;
+
+            //if element has this id then scroll to it
+            if ($(hash).length != 0) {
+                element = $(hash);
+            }
+            //catch cases of links that use anchor name
+            else if ($('a[name="' + hashName + '"]').length != 0)
+            {
+                //just use the first one in case there are multiples
+                element = $('a[name="' + hashName + '"]:first');
+            }
+
+            //if we have a target then go to it
+            if (element != undefined) {
+                window.scrollTo(0, element.position().top);
+            }
+            //unbind the scroll event
+            $(document).unbind("scroll");
+        });
+    }
+
+
 
 	$("#box-area h4").click(function() {
 		$(this).next().slideToggle();
@@ -22,6 +49,7 @@ $(document).ready(function() {
 				"top" : showcaseTop
 			}); //css
 	}
+
 
 	$(window).resize(function () {
 		location.reload();
