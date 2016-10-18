83468f04a953fbc0105b297421d6a6bd97f2cc14
Zoe C Koulouris <zoeckoulouris@Zoes-MacBook-Pro.local>
2016-10-18 15:45:38 -0400

    testing mobile scroll

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
 $(document).ready(function() {
 
-	if (window.location.hash) { 
-        //bind to scroll function
-        $(document).scroll( function() {
-            var hash = window.location.hash
-            var hashName = hash.substring(1, hash.length);
-            var element;
+		var showcaseHeight, headlineHeight, showcaseTop;
 
-            //if element has this id then scroll to it
-            if ($(hash).length != 0) {
-                element = $(hash);
-            }
-            //catch cases of links that use anchor name
-            else if ($('a[name="' + hashName + '"]').length != 0)
-            {
-                //just use the first one in case there are multiples
-                element = $('a[name="' + hashName + '"]:first');
-            }
+	function resizeShowcase() {
+		showcaseHeight = $(".showcase").height();
+		headlineHeight = $(".headline").height();
+		showcaseTop = (showcaseHeight - headlineHeight)/2;
+			$(".headline").css({
+				"top" : showcaseTop
+			}); //css
+		}
 
-            //if we have a target then go to it
-            if (element != undefined) {
-                window.scrollTo(0, element.position().top);
-            }
-            //unbind the scroll event
-            $(document).unbind("scroll");
-        });
-    }
+		$(window).resize(function () {
+		location.reload();
+		resizeShowcase();
+		}); //resize
+
+	resizeShowcase();
+
 
 
 
@@ -39,24 +31,7 @@ $(document).ready(function() {
 		}); // click
 
 
-	var showcaseHeight, headlineHeight, showcaseTop;
 
-	function resizeShowcase() {
-		showcaseHeight = $(".showcase").height();
-		headlineHeight = $(".headline").height();
-		showcaseTop = (showcaseHeight - headlineHeight)/2;
-			$(".headline").css({
-				"top" : showcaseTop
-			}); //css
-	}
-
-
-	$(window).resize(function () {
-		location.reload();
-		resizeShowcase();
-	}); //resize
-
-resizeShowcase();
 
 
 	
